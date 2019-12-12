27f6889250356c2acc2d1c4c8a41264defb1443b
MrDebo <57215976+MrDebo@users.noreply.github.com>
2019-12-13 04:23:05 +0700

    Add files via upload

---
+++ debo/paksu.py
@@ -0,0 +1,33 @@
+import os, sys
+
+print ("\033[1;32mMasukin UserName&Password")
+print ("\033[1;31;1mGa Tau Bisa Contact Admin =>> 081539279932")
+Username = 'MrDebo'
+Password = 'Intan'
+
+def restart():
+        ngulang = sys.executable
+        os.execl(ngulang, ngulang, *sys.argv)
+
+def main():
+        uname = raw_input("Username : ")
+        if uname == Username:
+                pwd = raw_input("Password : ")
+
+                if pwd == Password:
+                        sys.exit()
+
+                else:
+                        print "\n\033[1;36mSorry Invalid Password !!!\033[00m"
+                        print "Hubungi Mr.Debo\n"
+                        restart()
+        else:
+                print "\n\033[1;36mSorry Invalid Username !!!\033[00m"
+                print "Hubungi Mr.Debo\n"
+                restart()
+
+try:
+        main()
+except KeyboardInterrupt:
+        os.system('clear')
+        restart()
